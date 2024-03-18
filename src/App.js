3fb6ab11ec218118a99f83335907be76d813a86d
sachin-maker <sachindeshpande456@gmail.com>
2024-03-19 03:44:44 +0530

    imageSlider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ImageSlider from './components/ImageSlider';
 // import Accordian from './components/accordian/SingleAccordian';
 import StarRating from './components/star-ratings';
 
@@ -7,7 +8,9 @@ function App() {
     <div className="App">
       {/* <Accordian /> */}
 
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={20} />
     </div>
   );
 }
